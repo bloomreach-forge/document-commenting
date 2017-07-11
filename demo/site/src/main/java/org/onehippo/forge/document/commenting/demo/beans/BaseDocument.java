76a0060390ed9c7868b114903611fbe778df0bd6
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-07-11 15:14:26 -0400

    Adding new demo

---
+++ demo/site/src/main/java/org/onehippo/forge/document/commenting/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.document.commenting.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="doccommentingdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
