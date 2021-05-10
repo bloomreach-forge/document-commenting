9f0bd687a52baea71706f00edde2c4a932849a44
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2021-05-10 17:46:34 +0200

    HIPFORGE-405 re-adde /demo, new 14 version

---
+++ demo/site/components/src/main/java/org/bloomreach/forge/doccommentingdemo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.bloomreach.forge.doccommentingdemo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="doccommentingdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
