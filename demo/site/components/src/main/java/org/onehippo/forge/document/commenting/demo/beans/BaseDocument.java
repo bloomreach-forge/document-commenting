72cf66e68eaa9e17f2f1ac22aede82c2fe904b88
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2021-05-10 17:44:27 +0200

    HIPFORGE-405 remove /demo, has 13 version

--- demo/site/components/src/main/java/org/onehippo/forge/document/commenting/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.document.commenting.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="doccommentingdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
